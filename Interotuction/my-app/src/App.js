c17616cb23995fb09b9a0f4924e32294d1333fbf
FARTASH-421 <abdul.qhadir.fartash@gmail.com>
2025-05-09 15:30:26 +0330

    practise componet in react jsx

--- Interotuction/my-app/src/App.js
+++ Interotuction/my-app/src/App.js
@@ -1,12 +1,23 @@
-import './App.css';
+import "./App.css";
+import Emplyee from "./components/Employee";
 
 function App() {
+  const chcek = true;
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>
-            Hello Reacte
-        </h1>
+        {/* {console.log(chcek)} */}
+        {chcek ? (
+          <>
+            <Emplyee />
+            <Emplyee />
+            <Emplyee />
+            <Emplyee />
+          </>
+        ) : (
+          <h1>Hello Reacte</h1>
+        )}
       </header>
     </div>
   );
---
+++ Interotuction/my-app/src/components/Employee.jsx
@@ -0,0 +1,6 @@
+import "./myEmplay.css";
+function Emplyee() {
+  return <div className="myEmplay">Hello Emplay</div>;
+}
+
+export default Emplyee;
