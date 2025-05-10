0617ad363c2eb948c49474a7d0f287a8af5e79e6
FARTASH-421 <abdul.qhadir.fartash@gmail.com>
2025-05-11 00:14:59 +0330

    test Tailwind Fold Extention

--- Interotuction/my-app/src/components/Employee.jsx
+++ Interotuction/my-app/src/components/Employee.jsx
@@ -1,15 +1,19 @@
-import "./myEmplay.css";
 function Emplyee(props) {
   return (
     <div className="bg-green-300 p-10 border-2 border-b-teal-950 my-2 text-wrap font-bold text-3xl hover:bg-yellow-600">
       <div>
         {props.name ? (
-          <p className="ex"> Hello {props.name}</p>
+          <p className="bg-red-400 text-center rounded-full">
+            {" "}
+            Hello {props.name}
+          </p>
         ) : (
           <p className="noex"> no body</p>
         )}
       </div>
-      <h6 className="age">{props.age > 18 ? props.age : "It's a Child"}</h6>
+      <h6 className="text-center">
+        {props.age > 18 ? props.age : "It's a Child"}
+      </h6>
     </div>
   );
 }
