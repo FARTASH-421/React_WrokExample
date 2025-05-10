a2ea43eb4c9c8c8fb5cf51b5d122e0ed92167222
FARTASH-421 <abdul.qhadir.fartash@gmail.com>
2025-05-10 22:51:25 +0330

    somthing changed

--- Interotuction/my-app/src/App.js
+++ Interotuction/my-app/src/App.js
@@ -1,18 +1,30 @@
 import "./App.css";
 import Emplyee from "./components/Employee";
+import { useState } from "react";
 
 function App() {
   const chcek = true;
+  const [name, setName] = useState("Fartash");
+  // let name = "Fartash";
 
   return (
     <div className="App">
       <header className="App-header">
-        {/* {console.log(chcek)} */}
         {chcek ? (
           <>
-            <Emplyee />
-            <Emplyee />
-            <Emplyee />
+            <input
+              className="myInput"
+              placeholder="Enter name*"
+              type="text"
+              onChange={(e) => {
+                console.log(e.target.value);
+                setName(e.target.value);
+              }}
+            ></input>
+
+            <Emplyee name={name} age={21} />
+            <Emplyee name={"Abdul Qadir"} age={17} />
+            <Emplyee name={""} age={41} />
             <Emplyee />
           </>
         ) : (
--- Interotuction/my-app/src/components/Employee.jsx
+++ Interotuction/my-app/src/components/Employee.jsx
@@ -1,6 +1,17 @@
 import "./myEmplay.css";
-function Emplyee() {
-  return <div className="myEmplay">Hello Emplay</div>;
+function Emplyee(props) {
+  return (
+    <div className="myEmplay">
+      <div>
+        {props.name ? (
+          <p className="ex"> Hello {props.name}</p>
+        ) : (
+          <p className="noex"> no body</p>
+        )}
+      </div>
+      <h6 className="age">{props.age > 18 ? props.age : "It's a Child"}</h6>
+    </div>
+  );
 }
 
 export default Emplyee;
