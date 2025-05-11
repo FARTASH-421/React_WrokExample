c42412369c7fa9262da352176eeaa44e544d04d8
FARTASH-421 <abdul.qhadir.fartash@gmail.com>
2025-05-11 23:31:21 +0330

    work with card and map in react

--- Interotuction/my-app/src/App.js
+++ Interotuction/my-app/src/App.js
@@ -1,36 +1,68 @@
 import "./index.css";
 import Emplyee from "./components/Employee";
 import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
   const chcek = true;
-  const [name, setName] = useState("Fartash");
-  // let name = "Fartash";
+  const [employees, setEmployees] = useState([
+    {
+      name: "Fartash",
+      roule: "Maniger",
+      img: "https://cdn.pixabay.com/photo/2017/06/26/02/47/man-2442565_640.jpg",
+    },
+    {
+      name: "Qadir",
+      roule: "software Engeneer",
+      img: "https://cdn.pixabay.com/photo/2018/04/27/03/50/portrait-3353699_640.jpg",
+    },
+    {
+      name: "Hujat",
+      roule: "Disgner UX/UI",
+      img: "https://cdn.pixabay.com/photo/2015/08/31/10/07/man-915230_640.jpg",
+    },
+    {
+      name: "Samad",
+      roule: "Tester",
+      img: "https://cdn.pixabay.com/photo/2015/12/25/15/53/girl-1107788_960_720.jpg",
+    },
+    {
+      name: "Hadi",
+      roule: "software Engeneer",
+      img: "https://cdn.pixabay.com/photo/2023/11/18/09/12/ai-generated-8395968_640.jpg",
+    },
+    {
+      name: "Ali",
+      roule: "Boss",
+      img: "https://cdn.pixabay.com/photo/2019/07/13/02/25/man-4333898_640.jpg",
+    },
+    {
+      name: "Ali",
+      roule: "Boss",
+      img: "https://cdn.pixabay.com/photo/2019/07/13/02/25/man-4333898_640.jpg",
+    },
+    {
+      name: "Ali",
+      roule: "Boss",
+      img: "https://cdn.pixabay.com/photo/2019/07/13/02/25/man-4333898_640.jpg",
+    },
+  ]);
 
   return (
     <div className="App">
-      <header className="bg-blue-400 h-full w-full text-center">
-        {chcek ? (
-          <>
-            <input
-              className="myInput"
-              placeholder="Enter name*"
-              type="text"
-              onChange={(e) => {
-                console.log(e.target.value);
-                setName(e.target.value);
-              }}
-            ></input>
-
-            <Emplyee name={name} age={21} />
-            <Emplyee name={"Abdul Qadir"} age={17} />
-            <Emplyee name={""} age={41} />
-            <Emplyee />
-          </>
-        ) : (
-          <h1>Hello Reacte</h1>
-        )}
-      </header>
+      <div className="h-full w-full flex flex-wrap">
+        {employees.map((element) => {
+          console.log(uuidv4());
+          return (
+            <Emplyee
+              key={uuidv4()}
+              name={element.name}
+              role={element.roule}
+              img={element.img}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
--- Interotuction/my-app/src/components/Employee.jsx
+++ Interotuction/my-app/src/components/Employee.jsx
@@ -1,19 +1,24 @@
 function Emplyee(props) {
   return (
-    <div className="bg-green-300 p-10 border-2 border-b-teal-950 my-2 text-wrap font-bold text-3xl hover:bg-yellow-600">
-      <div>
-        {props.name ? (
-          <p className="bg-red-400 text-center rounded-full">
-            {" "}
-            Hello {props.name}
-          </p>
-        ) : (
-          <p className="bg-yellow-300 "> no body</p>
-        )}
+    <div className="h-[280px] w-[550px] p-5 bg-white rounded-3xl m-3 shadow-[0_0_10px_black] flex gap-2 hover:bg-green-200">
+      <div className="w-1/3 h-full flex justify-center items-center overflow-hidden">
+        <img
+          src={props.img}
+          alt="#"
+          className="bg-gray-400 h-48 w-64 rounded-full"
+        />
       </div>
-      <h6 className="text-center">
-        {props.age > 18 ? props.age : "It's a Child"}
-      </h6>
+      <div className="w-2/3 h-full p-5 flex gap-10 flex-col justify-center items-center">
+        <div className="text-black font-bold text-4xl font-serif">
+          <h1> {props.name}</h1>
+        </div>
+        <div className="text-gray-400 font-serif text-3xl">
+          <h2>{props.role}</h2>
+        </div>
+        <button className="h-20 w-32 rounded-3xl font-bold text-2xl font-serif border-2 border-gray-500 text-purple-600 hover:bg-gray-200">
+          Edit
+        </button>
+      </div>
     </div>
   );
 }
